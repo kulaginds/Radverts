c1ccb5048079a9d715aed6a6199ee6c30a7997d1
Dmitry Kulagin <kulaginds@gmail.com>
2017-02-02 02:20:36 +0300

    controllers moved to /api

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  resources :adverts
-  resources :categories
-  resources :users
+  scope '/api' do
+    resources :adverts
+    resources :categories
+    resources :users
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
